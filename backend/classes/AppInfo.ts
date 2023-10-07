14a96ecf628cef33f4b20abc28fec10d0442a72e
David Bauer <89662560+davbauer@users.noreply.github.com>
2023-10-08 00:27:44 +0200

    Update default websocket port

--- backend/classes/AppInfo.ts
+++ backend/classes/AppInfo.ts
@@ -3,7 +3,7 @@ import InterfaceAppInfo from '../models/InterfaceAppInfo.js';
 export default class {
 	static get(): InterfaceAppInfo {
 		return {
-			webSocketPort: Number(process.env.WEBSOCK_PORT),
+			webSocketPort: Number(process.env.WEBSOCK_PORT) || 81,
 			gitCommitId: process.env.COMMITID || 'undefined',
 			gitBranchName: process.env.BRANCH || 'undefined',
 			upime: process.uptime(),
