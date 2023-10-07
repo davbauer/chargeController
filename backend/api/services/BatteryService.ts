739f5ae8d5280c35fa4467db2515a0545d00f0ea
David Bauer <89662560+davbauer@users.noreply.github.com>
2023-10-07 16:55:44 +0200

    Retry renaming BatteryService

--- backend/api/services/BatteryService.ts
+++ backend/api/services/BatteryService.ts
@@ -1,6 +1,7 @@
 import ApiBase from './ApiBase.js';
 import ConfigFile from '../../classes/ConfigFile.js';
-import InterfaceBatEMSData from '../../models/InterfaceBatEMSData.js'
+import InterfaceBatEMSData from '../../models/InterfaceBatEMSData.js';
+
 export default class extends ApiBase {
 	static async getEMSDATA(): Promise<InterfaceBatEMSData> {
 		return this.getXML2JSON<InterfaceBatEMSData>(ConfigFile.read().BatteryHost, 'cgi/ems_data.xml');
