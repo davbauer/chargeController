56bdb3ab05867d54bac0e8b4245f7a4915731249
David Bauer <89662560+davbauer@users.noreply.github.com>
2023-10-07 17:42:09 +0200

    Recreate files

--- backend/api/services/BatteryService.ts
+++ backend/api/services/BatteryService.ts
@@ -3,7 +3,7 @@ import ConfigFile from '../../classes/ConfigFile.js';
 import InterfaceBatEMSData from '../../models/InterfaceBatEMSData.js';
 
 export default class extends ApiBase {
-	static async getEMSDATA(): Promise<InterfaceBatEMSData> {
-		return this.getXML2JSON<InterfaceBatEMSData>(ConfigFile.read().BatteryHost, 'cgi/ems_data.xml');
-	}
+    static async getEMSDATA(): Promise<InterfaceBatEMSData> {
+        return this.getXML2JSON<InterfaceBatEMSData>(ConfigFile.read().BatteryHost, 'cgi/ems_data.xml');
+    }
 }
--- backend/models/InterfaceBatEMSDATA.ts
+++ backend/models/InterfaceBatEMSDATA.ts
@@ -1,15 +1,15 @@
 export default interface InterfaceBatEMSData {
-	root: {
-		Timestamp: string;
-		id: string;
-		ChargerCount: string;
-		Description: string;
-		inverter: {
-			id: string;
-			var: Array<{
-				name: 'P' | 'SOC' | 'Capacity' | 'State' | 'OG' | 'UG';
-				value: string;
-			}>;
-		};
-	};
+    root: {
+        Timestamp: string;
+        id: string;
+        ChargerCount: string;
+        Description: string;
+        inverter: {
+            id: string;
+            var: Array<{
+                name: 'P' | 'SOC' | 'Capacity' | 'State' | 'OG' | 'UG';
+                value: string;
+            }>;
+        };
+    };
 }
