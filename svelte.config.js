940d6ad7f00a2ba01fd0710fbeaa8b2f1cf1df44
David Bauer <89662560+davbauer@users.noreply.github.com>
2024-01-05 19:17:56 +0100

    Ran sveltekit v2 migration script
    npx svelte-migrate@latest sveltekit-2

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { exec } from 'child_process';
 
 //exec("yarn backend");
