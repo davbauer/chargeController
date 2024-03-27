be57e4a600e00cabc52b8dc8735da59ff8e7396c
David Bauer <89662560+davbauer@users.noreply.github.com>
2024-03-27 21:55:51 +0100

    Added better UI with tooltips and better mode logs

--- src/lib/components/SectionModeSwitch.svelte
+++ src/lib/components/SectionModeSwitch.svelte
@@ -25,32 +25,36 @@
 	<div class="flex justify-center">
 		<div class="text-center join">
 			<button
+				data-tip="Force Off"
+				class="join-item btn tooltip"
 				on:click={() => onModeChange('force_off')}
-				class="join-item btn"
 				class:btn-active={$config.Mode === 'force_off'}
 				class:btn-primary={$config.Mode === 'force_off'}
 			>
 				<IconPower />
 			</button>
 			<button
+				data-tip="Sleep"
+				class="join-item btn tooltip"
 				on:click={() => onModeChange('sleep')}
-				class="join-item btn"
 				class:btn-active={$config.Mode === 'sleep'}
 				class:btn-primary={$config.Mode === 'sleep'}
 			>
 				<IconZzz />
 			</button>
 			<button
+				data-tip="Sun"
+				class="join-item btn tooltip"
 				on:click={() => onModeChange('sun')}
-				class="join-item btn"
 				class:btn-active={$config.Mode === 'sun'}
 				class:btn-primary={$config.Mode === 'sun'}
 			>
 				<IconSunHigh />
 			</button>
 			<button
+				data-tip="Sun+Force"
+				class="join-item btn tooltip"
 				on:click={() => onModeChange('sun_force')}
-				class="join-item btn"
 				class:btn-active={$config.Mode === 'sun_force'}
 				class:btn-primary={$config.Mode === 'sun_force'}
 			>
